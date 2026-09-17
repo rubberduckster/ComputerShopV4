4961e2467f8a452c6f0b90a615aaf3a422fd1979
rubberduckster <mcrubygirl@hotmail.com>
2026-09-17 21:41:00 +0200

    Add project files.

---
+++ ElectronicShopXML/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Linq;
+
+namespace ElectronicShopXML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument document = XDocument.Load("products.xml");
+
+            IEnumerable<XElement> products = document.Descendants("Product");
+
+            var computers = products.Where(product =>(string?)product.Element("Category") == "Computer");
+        }
+    }
+}
